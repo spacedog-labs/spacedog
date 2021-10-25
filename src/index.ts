eb45fc85e06017549b06ae20ff964f16a645a138
Colton <colathro@microsoft.com>
2021-10-24 19:07:29 -0700

    CheckBox -> Checkbox

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Theme from "./Theme/Theme";
 import Button from "./Button/Button";
 import OmegaButton from "./OmegaButton/OmegaButton";
 import Tree from "./Tree/Tree";
-import CheckBox from "./CheckBox/CheckBox";
+import Checkbox from "./Checkbox/Checkbox";
 import ChoiceGroup from "./ChoiceGroup/ChoiceGroup";
 import Input from "./Input/Input";
 import Label from "./Label/Label";
@@ -12,7 +12,7 @@ export {
   Button,
   OmegaButton,
   Tree,
-  CheckBox,
+  Checkbox,
   ChoiceGroup,
   Input,
   Label,

491fb62e811ff96fd5cdccc11692de77b1f64c17
Colton <colathro@microsoft.com>
2021-10-24 19:00:58 -0700

    0.1.2 - missing index.js exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import Theme from "./Theme/Theme";
 import Button from "./Button/Button";
 import OmegaButton from "./OmegaButton/OmegaButton";
+import Tree from "./Tree/Tree";
+import CheckBox from "./CheckBox/CheckBox";
+import ChoiceGroup from "./ChoiceGroup/ChoiceGroup";
+import Input from "./Input/Input";
+import Label from "./Label/Label";
 
-export { Theme, Button, OmegaButton };
+export {
+  Theme,
+  Button,
+  OmegaButton,
+  Tree,
+  CheckBox,
+  ChoiceGroup,
+  Input,
+  Label,
+};
